14c4f4df0181f5b29da0c7e475579c10980bc3f4
Shahoriar <shahoriar.fahim@gmail.com>
2023-04-13 13:09:22 +0600

    fix: add home link

--- LifeZone/Modules/OnlineCourses/Resources/views/userSide-Courses/allCourses.blade.php
+++ LifeZone/Modules/OnlineCourses/Resources/views/userSide-Courses/allCourses.blade.php
@@ -6,7 +6,7 @@
             <div class="container">
                 <div class="page-title-content">
                     <ul>
-                        <li><a href="index.html">Home</a></li>
+                        <li><a href="{{ route('home') }}">Home</a></li>
                         <li>Courses</li>
                     </ul>
                     <h2>Our Popular Online Courses</h2>
